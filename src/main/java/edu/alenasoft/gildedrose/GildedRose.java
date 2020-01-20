021798c98de5995828cae62e9d3b417fb3dc16ff
Robert Cabrera <clrobert.06@gmail.com>
2020-01-20 01:36:09 -0400

    Se agreó la nueva funcionalidad solicitada, para items CONJURED, se descomentaron los test de esta funcionalidad

--- src/main/java/edu/alenasoft/gildedrose/GildedRose.java
+++ src/main/java/edu/alenasoft/gildedrose/GildedRose.java
@@ -7,7 +7,9 @@ public class GildedRose {
 
   public static final String AGED_BRIE = "Aged Brie";
   public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-  public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+  public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT =
+      "Backstage passes to a TAFKAL80ETC concert";
+  public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
 
   public static final Integer ZERO = 0;
   public static final Integer SIX = 6;
@@ -47,6 +49,10 @@ public class GildedRose {
         backstagePassesCalculateQuality(item);
         break;
       case SULFURAS_HAND_OF_RAGNAROS:
+        break;
+      case CONJURED_MANA_CAKE:
+        normalItemCalculateQuality(item);
+        normalItemCalculateQuality(item);
         break;
       default:
         normalItemCalculateQuality(item);
